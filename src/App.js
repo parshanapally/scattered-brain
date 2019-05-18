602ff7d594a42c9e60b8a495f4a725eaa31273f7
Pooja <parshanap@gmail.com>
2019-05-18 08:28:44 -0500

    feat: create three routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Home from './components/Home/Home'
+import Calendar from './components/Calendar/Calendar'
+import Tasks from './components/Tasks/Tasks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+render() {
+    return (
+      <div>
+        <Router>
+            <Route path='/' component={Home} ></Route>
+            <Route path='/calendar/' component={Calendar}></Route>
+            <Route path='/tasks/' component={Tasks}></Route>
+        </Router>
+      </div>
+      
+       
+        
+    )
+}
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {expect} from 'code'
+import {shallow} from "enzyme"
+import App from './App'
+import Home from './components/Home/Home'
+import Calendar from './components/Calendar/Calendar'
+import Tasks from "./components/Tasks/Tasks"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("Given App", () => {
+    let component;
+
+  function requiredProps(overrides={})  {
+        return {
+            ...overrides
+        }
+  }
+
+  function renderComponent( props = requiredProps()) {
+        return shallow(<App {...props}/>)
+  }
+
+  beforeEach(() => {
+      component = renderComponent()
+  })
+
+  it("should exist", () => {
+   
+    expect(component).to.exist()
+  })
+
+  it('should render routes correctly', () => {
+
+      let Route = component.find('Route')
+      expect(Route).to.have.length(3)
+      expect(Route.at(0).prop('path')).to.equal('/')
+      expect(Route.at(0).prop('component')).to.equal(Home)
+
+      expect(Route.at(1).prop('path')).to.equal('/calendar/')
+      expect(Route.at(1).prop('component')).to.equal(Calendar)
+
+      expect(Route.at(2).prop('path')).to.equal('/tasks/')
+      expect(Route.at(2).prop('component')).to.equal(Tasks)
+
+
+  })
+})
