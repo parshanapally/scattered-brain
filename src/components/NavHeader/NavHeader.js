49ebaeb90f9f9bd504590b14215e9ae728cdd350
Pooja <parshanap@gmail.com>
2019-06-07 20:38:57 -0500

    style: navheader

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './NavHeader.css'
 
 
 const NavHeader = (props) => {
     return (
-        <div>
-            <Link to="/"> Home</Link>
+        <div class='navheader-container'>
             <Link to="/calendar/"> Calendar</Link>
             <Link to="/tasks/">Tasks</Link>
+            <Link to="/"> Home</Link>
+           
         </div>
     )
 }
